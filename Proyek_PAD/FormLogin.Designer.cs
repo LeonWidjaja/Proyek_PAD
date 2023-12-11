96679098517c20605172d899b30b3531569c46ae
mic-hb <michael.h22@mhs.istts.ac.id>
2023-12-11 21:24:38 +0700

    Added entity model

--- Proyek_PAD/FormLogin.Designer.cs
+++ Proyek_PAD/FormLogin.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Proyek_PAD
 {
-    partial class Form1
+    partial class FormLogin
     {
         /// <summary>
         /// Required designer variable.
@@ -42,42 +42,48 @@
             this.btnLogin.BackColor = System.Drawing.Color.DodgerBlue;
             this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLogin.ForeColor = System.Drawing.Color.White;
-            this.btnLogin.Location = new System.Drawing.Point(436, 200);
+            this.btnLogin.Location = new System.Drawing.Point(327, 162);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(257, 34);
+            this.btnLogin.Size = new System.Drawing.Size(193, 28);
             this.btnLogin.TabIndex = 17;
             this.btnLogin.Text = "Login";
             this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // tbPassword
             // 
-            this.tbPassword.Location = new System.Drawing.Point(520, 145);
+            this.tbPassword.Location = new System.Drawing.Point(390, 118);
+            this.tbPassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbPassword.Name = "tbPassword";
-            this.tbPassword.Size = new System.Drawing.Size(173, 22);
+            this.tbPassword.Size = new System.Drawing.Size(131, 20);
             this.tbPassword.TabIndex = 16;
             // 
             // tbUsername
             // 
-            this.tbUsername.Location = new System.Drawing.Point(520, 110);
+            this.tbUsername.Location = new System.Drawing.Point(390, 89);
+            this.tbUsername.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbUsername.Name = "tbUsername";
-            this.tbUsername.Size = new System.Drawing.Size(173, 22);
+            this.tbUsername.Size = new System.Drawing.Size(131, 20);
             this.tbUsername.TabIndex = 15;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(435, 148);
+            this.label3.Location = new System.Drawing.Point(326, 120);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(76, 16);
+            this.label3.Size = new System.Drawing.Size(62, 13);
             this.label3.TabIndex = 14;
             this.label3.Text = "Password : ";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(435, 113);
+            this.label2.Location = new System.Drawing.Point(326, 92);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(79, 16);
+            this.label2.Size = new System.Drawing.Size(64, 13);
             this.label2.TabIndex = 13;
             this.label2.Text = "Username : ";
             // 
@@ -85,9 +91,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(55, 94);
+            this.label1.Location = new System.Drawing.Point(41, 76);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(101, 38);
+            this.label1.Size = new System.Drawing.Size(85, 31);
             this.label1.TabIndex = 12;
             this.label1.Text = "Login";
             this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -95,17 +102,18 @@
             // label4
             // 
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(62, 148);
+            this.label4.Location = new System.Drawing.Point(46, 120);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(250, 108);
+            this.label4.Size = new System.Drawing.Size(188, 88);
             this.label4.TabIndex = 18;
             this.label4.Text = "Type your username and password to login to the sales menu :D";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.tbPassword);
@@ -113,6 +121,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
---
+++ Proyek_PAD/FormLogin.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyek_PAD
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = tbUsername.Text;
+            string password = tbPassword.Text;
+
+            if(username == "" || password == "")
+            {
+                MessageBox.Show("Username dan password tidak boleh kosong!");
+                return;
+            }
+
+            if (username == "admin")
+            {
+                if(password != "admin")
+                {
+                    MessageBox.Show("Password salah!");
+                    return;
+                }
+            }
+
+            AdventureWorks2019Entities context = new AdventureWorks2019Entities();
+            SalesPerson user = from SalesPerson in context.SalesPersons
+                               join Person in context.Persons on SalesPerson.BusinessEntityID equals Person.BusinessEntityID
+                               where Person.EmailPromotion == 1 && Person.EmailAddress == username
+        }
+    }
+}
